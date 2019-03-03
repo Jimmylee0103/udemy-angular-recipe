8796df21e1e88b3c2caf496ed212b463ac0798ab
Jimmy Lee <Jin_Lee2@Dell.com>
2019-03-02 23:30:25 -0600

    added shopping-list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
 
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
@@ -7,11 +8,20 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe',
       'This is a test description',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg')
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg',
+      [
+        new Ingredient('Bread', 1),
+        new Ingredient('Milk', 2)
+      ])
       ,
       new Recipe('A Test Recipe 2',
       'This is a test description 2',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg')];
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg',
+      [
+        new Ingredient('Juice', 2),
+        new Ingredient('Pickles', 1)
+      ])
+    ];
 
 
   getRecipes(){
