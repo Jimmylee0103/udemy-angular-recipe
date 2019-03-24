d7e8a936ddd9297e32caf6a8198f44b81be454ec
Jimmy Lee <Jin_Lee2@Dell.com>
2019-03-23 22:28:47 -0500

    add routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -12,13 +11,9 @@ export class RecipeItemComponent implements OnInit {
   // @Output() selectedRecipe = new EventEmitter<void>();
 
   @Input() recipe: Recipe;
-
-  constructor(private recipeSerive: RecipeService) { }
+  @Input() index: number;
 
   ngOnInit() {
   }
 
-  onSelectRecipeItem(){
-    this.recipeSerive.selectedRecipe.emit(this.recipe);
-  }
 }
